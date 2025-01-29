0a8cd7d17a854fae4fdc04e1d62ec405de26be47
Ammar <esrawiaammar22@gmail.com>
2025-01-29 14:15:46 +0300

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/shopCategory";
 import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/kids" element={<ShopCategory category="kids" />} />
           <Route path="/product" element={<Product />} />
             <Route path=':productId' element={<Product/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
         </Routes>
       </BrowserRouter>
     </div>
